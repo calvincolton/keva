a269407186be84afc3aad96dfe9b9d4a0595f84d
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2023-03-26 21:55:12 -0700

    transaction fix and transaction test

--- transact.go
+++ transact.go
@@ -28,32 +28,21 @@ type Event struct {
 // 	WritePut(key, value string)
 // 	Err() <-chan error
 
-// 	ReadEvents() (<-chan Event, <-chan error)
-
-// 	Run()
-// }
-
-// type TransactionLogger struct {
-// 	events       chan<- Event
-// 	errors       <-chan error
-// 	lastSequence uint64
-// 	file         *os.File
-// }
-
 type TransactionLogger struct {
-	events       chan<- Event
+	events       chan<- Event // Write-only channel for sending events
 	errors       <-chan error
-	lastSequence uint64
-	file         *os.File
-
-	wg *sync.WaitGroup
+	lastSequence uint64   // The last used event sequence number
+	file         *os.File // The location of the transaction log
+	wg           *sync.WaitGroup
 }
 
 func (l *TransactionLogger) WritePut(key, value string) {
-	l.events <- Event{EventType: EventPut, Key: key, Value: value}
+	l.wg.Add(1)
+	l.events <- Event{EventType: EventPut, Key: key, Value: url.QueryEscape(value)}
 }
 
 func (l *TransactionLogger) WriteDelete(key string) {
+	l.wg.Add(1)
 	l.events <- Event{EventType: EventDelete, Key: key}
 }
 
@@ -65,6 +54,7 @@ func NewTransactionLogger(filename string) (*TransactionLogger, error) {
 	var err error
 	var l TransactionLogger = TransactionLogger{wg: &sync.WaitGroup{}}
 
+	// Open the transaction log file for reading and writing.
 	l.file, err = os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
 	if err != nil {
 		return nil, fmt.Errorf("cannot open transaction log file: %w", err)
@@ -132,7 +122,6 @@ func (l *TransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
 				line, "%d\t%d\t%s\t%s",
 				&e.Sequence, &e.EventType, &e.Key, &e.Value)
 
-			// Sanity check! Are the sequence numbers is increasing order?
 			if l.lastSequence >= e.Sequence {
 				outError <- fmt.Errorf("transaction numbers out of sequence")
 				return
